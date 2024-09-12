ceadf57248778f54187548a9b0d3b706ecc18765
tiken14 <tiken14@live.com>
2024-09-12 19:11:35 +0500

    Верстка, отображения готовы

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return render(request, 'catalog/success.html')
+
+    return render(request, 'catalog/contacts.html')
+
