fe9b78deda63d8c30fc408d5f2924bb943d739eb
tiken14 <tiken14@live.com>
2024-09-17 20:25:45 +0500

    Работа над моделями. Установка всех необходимых библиотек

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    descr = models.TextField(verbose_name="Описание")
+    img = models.ImageField(verbose_name='Изображение')
+    category = models.ForeignKey(to='Category',
+                                 on_delete=models.CASCADE,
+                                 verbose_name='Категория',
+                                 related_name='products')
+
+    price = models.IntegerField(verbose_name='Цена')
+    created_at = models.DateTimeField(verbose_name='Время создания', default='now')
+    updated_at = models.DateTimeField(verbose_name='Время последнего изменения')
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        vebose_name = 'Продукт'
+        vebose_name_plural = 'Продукты'
+        ordering = ['name']
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    descr = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        vebose_name = 'Категория'
+        vebose_name_plural = 'Категории'
+        ordering = ['name']
+
+
--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pqwrzd^)k18e*hp)dzxreqe_-jef@z_0ug_4#+zu8w&(+ujlct'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -77,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": os.getenv('ENGINE'),
+        "NAME": os.getenv("NAME"),
+        "USER": os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
