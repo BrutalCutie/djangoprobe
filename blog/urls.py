11790923845dce5a0ccfdbab3b007ceadbe4deae
tiken14 <tiken14@live.com>
2024-10-04 16:57:50 +0500

    Изменения дизайна отображения

--- blog/urls.py
+++ blog/urls.py
@@ -8,7 +8,7 @@ app_name = BlogConfig.name
 urlpatterns = [
     path('', views.BlogListView.as_view(), name='home'),
     path('new/', views.BlogCreateView.as_view(), name='new_blog'),
-    path('detail/<int:pk>', views.BlogDetailView.as_view(), name='blog_detail'),
-    path('update/<int:pk>', views.BlogUpdateView.as_view(), name='blog_update'),
-    path('delete/<int:pk>', views.BlogDeleteView.as_view(), name='blog_delete'),
+    path('detail/<int:pk>/', views.BlogDetailView.as_view(), name='blog_detail'),
+    path('update/<int:pk>/', views.BlogUpdateView.as_view(), name='blog_update'),
+    path('delete/<int:pk>/', views.BlogDeleteView.as_view(), name='blog_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import DetailView, ListView, TemplateView
 from django.views.generic.edit import DeleteView, CreateView, UpdateView
 from .models import BlogModel
@@ -9,11 +9,23 @@ class BlogListView(ListView):
     template_name = "blog/index.html"
     context_object_name = 'blogs'
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(is_active=True)
+
+
+
 
 class BlogDetailView(DetailView):
     model = BlogModel
     template_name = 'blog/blog_detail.html'
     context_object_name = 'blog'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.views += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -27,7 +39,9 @@ class BlogUpdateView(UpdateView):
     model = BlogModel
     template_name = "blog/new_blog.html"
     fields = ['title', 'inner', 'preview', 'is_active']
-    success_url = reverse_lazy("blog:home")
+
+    def get_success_url(self):
+        return reverse("blog:blog_detail", kwargs={'pk': self.object.pk})
 
 
 class BlogDeleteView(DeleteView):
